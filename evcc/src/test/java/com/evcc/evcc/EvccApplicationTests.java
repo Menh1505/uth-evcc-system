9a84b200201bbaf63a4df77beab2c2987cce85de
Menhythien <dinhthienmenh1505@gmail.com>
2025-10-13 23:25:19 +0700

    init source

---
+++ evcc/src/test/java/com/evcc/evcc/EvccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evcc.evcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
