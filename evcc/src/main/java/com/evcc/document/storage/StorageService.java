1d09eb16fd08ce524f0d5f5f8bcf0977f23c72c9
Anh Lan <anhhnl7883@ut.edu.vn>
2025-11-02 14:42:05 +0700

    feat(storage): add StorageService

--- evcc/src/main/java/com/evcc/document/storage/StorageService.java
+++ evcc/src/main/java/com/evcc/document/storage/StorageService.java
@@ -1,5 +1,8 @@
 package com.evcc.document.storage;
 
-public class StorageService {
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+    String storeFile(MultipartFile file);
 
 }
