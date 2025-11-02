273b96b7024ce4b8c98de899b7f511c6a1eeee0b
Anh Lan <anhhnl7883@ut.edu.vn>
2025-11-02 14:31:04 +0700

    feat(repository): add DocumentUploadRepository

--- evcc/src/main/java/com/evcc/document/repository/DocumentUploadRepository.java
+++ evcc/src/main/java/com/evcc/document/repository/DocumentUploadRepository.java
@@ -1,5 +1,8 @@
 package com.evcc.document.repository;
+import com.evcc.document.model.DocumentUpload;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class DocumentUploadRepository {
-
+@Repository
+public interface DocumentUploadRepository extends JpaRepository<DocumentUpload, Long> {
 }
