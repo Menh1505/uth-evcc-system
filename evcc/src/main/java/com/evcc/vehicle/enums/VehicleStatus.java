7d4f045c9d79e9923e539e5c958359bafaa15ca6
Menhythien <dinhthienmenh1505@gmail.com>
2025-11-03 21:48:14 +0700

    Vehicle: create enums

---
+++ evcc/src/main/java/com/evcc/vehicle/enums/VehicleStatus.java
@@ -0,0 +1,12 @@
+package com.evcc.vehicle.enums;
+
+/**
+ * Trạng thái của xe điện
+ */
+public enum VehicleStatus {
+    AVAILABLE,
+    IN_USE,
+    CHARGING,
+    MAINTENANCE,
+    DECOMMISSIONED
+}
