53817e3bfc5bb8c0fa28475a34dff3778c90d97d
Anh Lan <anhhnl7883@ut.edu.vn>
2025-11-03 22:57:17 +0700

    Update VehicleController.java

--- evcc/src/main/java/com/evcc/vehicle/controller/VehicleController.java
+++ evcc/src/main/java/com/evcc/vehicle/controller/VehicleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +35,7 @@ public class VehicleController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<VehicleResponse> createVehicle(@Valid @RequestBody VehicleCreateRequest req) {
         VehicleResponse resp = vehicleService.createVehicle(req);
         return ResponseEntity.status(HttpStatus.CREATED).body(resp);
